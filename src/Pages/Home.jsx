dd34c64f9b9af6283df677fe82222760b7b180f7
rawr <85380493+Tralphue@users.noreply.github.com>
2026-06-28 23:15:54 +0800

    change/swap the animation img/svg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -210,7 +210,7 @@ const Home = () => {
                 }`}>
 
                   <img 
-                    src="Animation1.gif" 
+                    src="Animation2.svg" 
                     alt="Developer Animation" 
                     className="w-full h-full object-contain transition-all duration-500 scale-[95%] sm:scale-[90%] md:scale-[90%] lg:scale-[90%] rotate-2"
                   />
